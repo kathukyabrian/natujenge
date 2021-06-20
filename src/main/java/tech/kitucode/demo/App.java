f833090de5dd1a5ae7ba8118ac4e7c117f889574
brian <briankathukya2000@gmail.com>
2021-06-20 11:08:01 +0300

    fetch capital cities

--- src/main/java/tech/kitucode/demo/App.java
+++ src/main/java/tech/kitucode/demo/App.java
@@ -1,9 +1,72 @@
 package tech.kitucode.demo;
 
-public class App 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+public class App
 {
-    public static void main( String[] args )
+
+    public static void main(String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        int port = 12000;
+
+        final Map<String, String> countries = new HashMap<String, String>();
+
+        countries.put("Kenya","Nairobi");
+        countries.put("Tanzania","Dodoma");
+        countries.put("Uganda","Kampala");
+        countries.put("Somalia","Mogadishu");
+
+        System.out.println(countries);
+
+        try {
+            final ServerSocket serverSocket = new ServerSocket(port);
+
+            System.out.println("demo|server connected with port : "+serverSocket.getLocalPort());
+
+            System.out.println("demo|waiting for connections:");
+
+            while(true){
+
+                final Socket socket = serverSocket.accept();
+
+                System.out.println("demo|starting a connection to : "+socket.getInetAddress() + " at port : "+socket.getPort());
+
+                Thread thread = new Thread(new Runnable() {
+                    public void run() {
+                        try {
+
+                            System.out.println("demo|client connected with port : "+socket.getPort());
+
+                            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+
+                            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                            String inputLine;
+
+                            while((inputLine=in.readLine())!=null){
+                                out.println(countries.get(inputLine.substring(0,1).toUpperCase() + inputLine.substring(1).toLowerCase()));
+                                System.out.println(countries.get(inputLine.substring(0,1).toUpperCase() + inputLine.substring(1).toLowerCase()));
+                            }
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                thread.start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
